38dacdce30e168b62e84c1b58b7515d460c89b76
Mariana Frazão <mariana.franco.frazao@gmail.com>
2024-11-12 13:49:35 +0000

    implemented models

--- server/app.js
+++ server/app.js
@@ -3,10 +3,23 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const cohorts = require("./cohorts.json")
 const students = require("./students.json")
-
+const Student = require("./models/Student.model");
+const Cohort = require("./models/Cohort.model");
 const cors = require("cors");
 
+
+const mongoose = require("mongoose");
+
+mongoose
+.connect("mongodb://127.0.0.1:27017/mongoose-intro-dev")
+.then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+.catch(err => console.error("Error connecting to mongo", err));
+
+
+
 const PORT = 5005;
+
+
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
