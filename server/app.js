1ad0a16a0b57190ab3e1fd19d2aee97fa27170c4
Justin Fanton <justin.fanton@gmail.com>
2024-11-14 15:41:45 +0100

    Installed express packages (Justin), modified mongoose connect

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 mongoose
-.connect("mongodb://127.0.0.1:27017/mongoose-intro-dev")
+.connect("mongodb://127.0.0.1:27017/cohort-tools-api")
 .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
 .catch(err => console.error("Error connecting to mongo", err));
 
