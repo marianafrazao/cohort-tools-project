b7d9f23be9ea602e9259b5be27e3f408d2ab5314
Justin Fanton <justin.fanton@gmail.com>
2024-11-15 15:01:02 +0100

    Corrected user model syntax

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const { Schema, model } = mongoose;
 
-const userShema = new Schema({
+const userSchema = new Schema({
     email: {
         type: String,
         unique: true,
